a948346654768c24d1376a6cf3a912e82fc35d22
Jesavid <jesusdavida.e2089@gmail.com>
2024-10-08 15:52:17 -0600

    Agregar archivos de proyecto.

---
+++ Gmail/GmailAccountCreator.cs
@@ -0,0 +1,25 @@
+﻿namespace Gmail
+{
+    public class GmailAccountCreator{
+
+        private string nombre {  get; set; }
+        private string apellido { get; set; }
+        // fecha_nacimiento;
+        private List<string> sexo { get; set; }
+
+
+        // Validar nombre [Longitud, caracteres especiales]
+        // Validar campos vacios
+        // Validar fecha [Numeros, rango de fechas [No puede ser el mismo dia por la edad]]
+        // Validar genero
+        // Validar correo [Longitud (Entre 6 y 30 caracteres, Caracteres especiales (Letras, números y signos de puntuación))]
+        // Validar contraseña [Caracteres especiales, mayusculas y minusculas, Campo 'Confirmar contraseña' igual, Longitud (mayor a 8), contraseñas sencillas]
+        // Número de telefono [Lada (ladas que existan), Longitud de número (10 números), NO CADENAS]
+        // Código de verificacion
+
+
+
+
+
+    }
+}
---
+++ GmailTest/GmailTest.cs
@@ -0,0 +1,11 @@
+namespace GmailTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1(){
+
+        }
+    }
+}
